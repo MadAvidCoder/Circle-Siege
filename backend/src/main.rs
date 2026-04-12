a180cc970983428b73aef63936607540f1db16a0
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2026-04-12 23:26:27 +1000

    Stereo mixdown
    Added handlers for non-mono tracks, to mix them down if needed

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,6 +3,7 @@ use serde::{Serialize};
 use std::fs::File;
 use std::io::{BufWriter, Write};
 use hound;
+use itertools::Itertools;
 
 #[derive(Parser)]
 #[command(name = "CircleSiegeBackend")]
@@ -26,11 +27,14 @@ struct AnalyzeArgs {
     output: String,
 }
 
+
 #[derive(Serialize)]
 #[serde(tag = "type")]
 enum Record {
     #[serde(rename = "meta")]
     Meta(MetaRecord),
+    #[serde(rename = "energy")]
+    Energy(EnergyRecord),
     #[serde(rename= "done")]
     Done,
 }
@@ -44,16 +48,35 @@ struct MetaRecord {
     duration: f64,
 }
 
+#[derive(Serialize)]
+struct EnergyRecord {
+    t: f64,
+    e: f32
+}
+
 fn main() -> anyhow::Result<()> {
     let args = CLI::parse();
     match args.command {
         Commands::AnalyzeWav(params) => {
             let mut reader = hound::WavReader::open(&params.input).unwrap();
-            let samples: Vec<i16> = reader
-                .samples()
-                .map(|s| s.unwrap())
-                .collect();
             let spec = reader.spec();
+
+            let samples: Vec<i16> = reader
+                .samples::<i16>()
+                .chunks(spec.channels as usize)
+                .into_iter()
+                .map(|chunk| {
+                    let mut sum: i32 = 0;
+                    let mut count: i32 = 0;
+                    for s in chunk {
+                        if let Ok(sample) = s {
+                            sum += sample as i32;
+                            count += 1
+                        }
+                    }
+                    (sum / count) as i16
+                })
+                .collect();
 
             let file = File::create(&params.output)?;
             let mut w = BufWriter::new(file);
